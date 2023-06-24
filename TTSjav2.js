eef5229f69365518eb7b7a0b5435839f2f707280
yKbral <133119202+yKbral@users.noreply.github.com>
2023-06-24 15:27:40 -0300

    Add files via upload

---
+++ TTSjav2.js
@@ -0,0 +1,10 @@
+alert("Olá seja bem vindo, por fvaor responda as sgeuintes perguntas para efetuarmos seu login")
+const nome = ("qual o seu nome");
+alert (nome);
+const cidade = prompt("Digite a sua cidade:");
+alert (cidade);
+const idade = prompt("Por favor digite sua idade");
+alert (idade);
+const ling = prompt("Qual a liinguaguem de programação você está fazendo?");
+alert (ling);
+const msg = `Olá ${nome},Você é de ${cidade}, e possui ${idade}anos e está estudando ${ling} `;
